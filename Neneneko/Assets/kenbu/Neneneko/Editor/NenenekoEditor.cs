85e9a85ce12edb2524d6461b2eecea5a2e9ea8f5
高橋 健一 <takahashi_kenichi@cyberagent.co.jp>
2017-05-12 11:17:26 +0900

    エディタから再生

---
+++ Neneneko/Assets/kenbu/Neneneko/Editor/NenenekoEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;                              
+using UnityEngine;
+using kenbu.Neneneko;
+[CustomEditor(typeof(Neneneko))]               
+public class NenenekoEditor : Editor          
+{
+
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Neneneko neneneko = target as Neneneko;
+
+
+        if( GUILayout.Button( "Play" ) )
+        {
+            neneneko.Play ();
+        }
+        if( GUILayout.Button( "Stop" ) )
+        {
+            neneneko.Stop ();
+        }
+
+    }
+
+
+
+}

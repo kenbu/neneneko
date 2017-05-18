33688fae49c889e2f21376531a063ccdf55e8fa7
高橋 健一 <takahashi_kenichi@cyberagent.co.jp>
2017-05-18 16:06:16 +0900

    ci

---
+++ ci/ci.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -logFile ${WORKSPACE}Editor.log -projectPath ${WORKSPACE}/Neneneko -executeMethod NenenekoCI.StartTest
+
+result=$?
+
+
+echo ${result}
+
+if [ "$result" = "1" ]
+then
+	echo "問題あり"
+	#一旦固定パスで動画生成
+	echo "----動画出力----"
+	ffmpeg -i ${WORKSPACE}/Neneneko/Assets/kenbu/Neneneko/Captured/%d.png -pix_fmt yuv420p ${WORKSPACE}/Neneneko/Assets/kenbu/Neneneko/Captured/result.mp4
+
+	#エラーを返す
+
+	#todo: ログなんか
+	return 1
+else
+	echo "問題なし"
+fi
+
+return 0
